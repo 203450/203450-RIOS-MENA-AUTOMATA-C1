e6c237ff35ff6cb2ee289e1c1ef6a45a83da748d
203450 <77992818+203450@users.noreply.github.com>
2022-10-13 22:04:06 -0500

    updated

--- interfaz.py
+++ interfaz.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import tkinter as tk
 import tkinter.filedialog as fd
 import tkinter.messagebox as mb
@@ -12,12 +13,7 @@ class Aplicacion(tk.Tk):
         
     def inicializar_gui(self):
         self.title('Selector de archivos y carpetas')
-        self.geometry('650x400')
-        
-        btn_seleccionar_carpeta = tk.Button(self, text='Seleccionar carpeta para guardar el reporte...')
-        btn_seleccionar_carpeta['command'] = self.seleccionar_carpeta
-        
-        btn_seleccionar_carpeta.pack(padx=150, pady=60)
+        self.geometry('600x550')
         
         btn_seleccionar_archivo = tk.Button(self, text='Seleccionar archivo...')
         btn_seleccionar_archivo['command'] = self.seleccionar_archivo
@@ -27,7 +23,7 @@ class Aplicacion(tk.Tk):
         
     def seleccionar_archivo(self):
         archivo = fd.askopenfilename(
-            initialdir='D:/Archivos/Tareas/Autómatas/Automatac1/203450-RIOS-MENA-AUTOMATA',
+            initialdir='./',
             title='Seleccionar archivo',
             filetypes=(('Archivos de texto', '*.txt'),)
         )
@@ -42,11 +38,16 @@ class Aplicacion(tk.Tk):
             btn_validar['command'] = lambda:m.main(archivo)
             btn_validar.pack(padx=60, pady=10)
             
+            btn_seleccionar_carpeta = tk.Button(self, text='Seleccionar carpeta para guardar el reporte...')
+            btn_seleccionar_carpeta['command'] = self.seleccionar_carpeta
+            
+            btn_seleccionar_carpeta.pack(padx=150, pady=60)
+                
     
     def seleccionar_carpeta(self):
         
         directorio = fd.askdirectory(
-            initialdir='D:/Archivos/Tareas/Autómatas/Automatac1/203450-RIOS-MENA-AUTOMATA',
+            initialdir='./',
             title='Seleccionar carpeta'
         )
         
@@ -55,11 +56,16 @@ class Aplicacion(tk.Tk):
         if directorio:
             mb.showinfo('Carpeta seleccionada: ', directorio)
             
-            btn_validar = tk.Button(self, text='Generar reporte')
-            btn_validar['command'] = lambda:m.reporte_excel(directorio)
-            btn_validar.pack(padx=150, pady=10)
+            btn_generar = tk.Button(self, text='Generar reporte')
+            btn_generar['command'] = lambda:self.reporte(directorio)
+            
+            btn_generar.pack(padx=150, pady=10)
+    
+    def reporte(self, directorio):
+        m.reporte_excel(directorio)
+        mb.showinfo('Reporte: ', 'Generado correctamente')
         
-
+        
 def main():
     app = Aplicacion()
     app.mainloop()
